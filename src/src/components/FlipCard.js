1be1547b0ab24d8a8e4194dfcd2c41a30efcc3c6
nbpillai <nipillai@ucsd.edu>
2023-04-22 14:41:27 -0700

    cards styled besides color change

--- src/src/components/FlipCard.js
+++ src/src/components/FlipCard.js
@@ -1,44 +1,36 @@
 import "../styles/flip-card.css";
 import { useState } from "react";
 
-export default function FlipCard() {
+export default function FlipCard({title, description, bullet1, bullet2, bullet3, icon, color, buttonText}) {
 	const [flip, setFlip] = useState(false);
-
-	// const flipToBack = () => {
-	// 	setFlip(1);
-	// 	console.log("flipped to back");
-	// }
-
-	// const flipToFront = () => {
-	// 	setFlip(0);
-	// 	console.log("flipped to front");
-	// }
 
 	return (
 		<div className="flip-card" onClick={ () => setFlip(!flip) }>
-			<div className={`flip-card-back ${flip ? "flipped": ""}`}>Back</div>
-			<div className={`flip-card-front ${flip ? "flipped": ""}`}>Front</div>
+			<div className={`flip-card-back ${flip ? "flipped": ""}`}>
+				<h1>
+					{title}
+				</h1>
+				<p>
+					{description}
+				</p>
+				<div className="bullets">
+				<ul>
+					<li>{bullet1}</li>
+					<li>{bullet2}</li>
+					<li>{bullet3}</li>
+				</ul>
+				</div>  
+				
+			</div>
+			<div className={`flip-card-front ${flip ? "flipped": ""}`}>
+				<h1>
+				{title}
+				</h1>
+				<div class="icon">
+				{icon}
+				</div>
+			</div>
 		</div>
 
-			// <div className="flip-card" onClick={ () => setFlip(!flip) }>
-			// 	<div className={`flip-card-back ${flip ? "flipped": ""}`}>
-			// 		{flip ? "flipped" : "not"}
-			// 	</div>
-			// 	<div className={`flip-card-front ${flip ? "flipped": ""}`}>
-			// 		{flip ? "flipped" : "not"}
-			// 	</div>
-			// </div>
-
-					// <div className={`flip-card ${flip === 1 ? "flip": "not-flip"}`}>
-		// 	<div className="flip-card-inner">
-		// 		<div className="flip-card-front" onClick={ flipToBack }>
-		// 			<p>card front</p>
-		// 		</div>
-		// 		<div className="flip-card-back" onClick={ flipToFront }>
-		// 			<p>card back</p>
-		// 		</div>
-		// 	</div>
-		// </div>
-		// <div className="flip-card" onClick={ () => setFlip(!flip) }>
 	);
 }
