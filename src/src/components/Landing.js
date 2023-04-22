d80025ccbd02f7aede8147eb9c0588d35a1c3fc0
Hannah Coates <hcoates@ucsd.edu>
2023-04-22 14:08:24 -0700

    made landing

---
+++ src/src/components/Landing.js
@@ -0,0 +1,20 @@
+import "../styles/landing.css";
+import { useState } from "react";
+
+export default function Landing() {
+
+
+
+	return (
+        <>
+        <div className="landing">
+            <br></br>
+                <h1 className="header" >Landing Page</h1>
+                <p>idk we can put like an about page or something</p>
+                <button classname="login"><b>Login / Sign Up</b></button>
+            
+        </div>
+
+        </>
+	);
+}
--- src/src/screens/Guides.js
+++ src/src/screens/Guides.js
@@ -1,12 +1,18 @@
 import "../styles/guides.css";
 import FlipCard from "../components/FlipCard";
+import Landing from  "../components/Landing";
 
 export default function Guides() {
 	return (
+		<>
+		<Landing></Landing>
+		<br></br>
 		<div className="Guides">
+			
 			<FlipCard></FlipCard>
 			<FlipCard></FlipCard>
 			<FlipCard></FlipCard>
 		</div>
+		</>
 	);
 }
