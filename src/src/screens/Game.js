02aa6130c3fa9b5a79920c5b083ed16c4b1398fb
Hannah Coates <hcoates@ucsd.edu>
2023-04-23 02:37:37 -0700

    game

---
+++ src/src/screens/Game.js
@@ -0,0 +1,10 @@
+import "../styles/game.css";
+import MatchGame2 from "../components/Cards";
+
+export default function Game() {
+	return (
+		<div className ="game">
+            <MatchGame2 />
+        </div>
+	);
+}
