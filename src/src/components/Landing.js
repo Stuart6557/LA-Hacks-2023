6fac3a07303dbada5e8e33e09846cf5cb819b948
jennymar <jenmar528@gmail.com>
2023-04-23 02:07:08 -0700

    landing page text

--- src/src/components/Landing.js
+++ src/src/components/Landing.js
@@ -5,9 +5,15 @@ import "../styles/landing.css";
 export default function Landing() {
 	return (
 		<div className="landing">
-			<br></br>
-			<h1 className="header" >Landing Page</h1>
-			<p>idk we can put like an about page or something</p>
+			<h1 className="header" >TITLE NAME HERE</h1>
+			<p>..so you're here to save to planet. perfect. </p>
+			<p>with increasing awareness and consciousness towards our environment
+				and Earth, many mindful humans, like you, have taken this issue into
+				our own hands. one of the easiest contributions we can all make is 
+				recycling! done correctly, recycling conserves trees, water, and minerals, 
+				as well as diminishes greenhouse gases and carbon emissions. learn more 
+				about how to dispose of recycling and nonrecycling properly by joining INSERT
+				TEAM NAME now!</p>
 			<Link to="/login">
 				<button classname="login"><b>Login / Sign Up</b></button>
 			</Link>
